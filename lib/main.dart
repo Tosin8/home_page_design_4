57892c47c3b4aaac10e3cabea844364a548966f0
Tosin8 <tosinezekiel8@gmail.com>
2022-06-28 17:40:55 +0100

    updating the main.dart with scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,34 @@ class _HomePageState extends State<HomePage>
   late Animation<double> _animation;
   late Animation<double> _animation2;
   @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 1)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut))
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _animation2 = Tween<double>(begin: -30, end: 0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+    _controller.forward();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
