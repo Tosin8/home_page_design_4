f3ab870c6e947777a72e0d0113b207a1317d8722
Tosin8 <tosinezekiel8@gmail.com>
2022-06-28 18:02:58 +0100

    including the HomePageCardsGroup function

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,99 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double w = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            ListView(
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(w / 17, w / 20, 0, w / 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Your App\'s Nmae',
+                            style: TextStyle(
+                              fontSize: 27,
+                              color: Colors.black.withOpacity(.6),
+                              fontWeight: FontWeight.w700,
+                            )),
+                        SizedBox(height: w / 35),
+                        Text(
+                          'Here you can write something about the app',
+                          style: TextStyle(
+                            fontSize: 19,
+                            color: Colors.black.withOpacity(.5),
+                            fontWeight: FontWeight.w500,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    )), 
+                    HomePageCardsGroup(
+                      const Color(0xfff37736), 
+                      Icons.analytics_outlined, 
+                      'Example example', 
+                      context, 
+                      RouteWhereYouGo(), 
+                      const Color(0xffFF6D6D), 
+                      Icons.all_inclusive, 
+                      'Example example example', 
+                      RouteWhereYouGo(), 
+                    ),
+                    HomePageCardsGroup(
+                      Colors.lightGreen, 
+                      Icons.gamepad_outlined,  
+                      'Example example', 
+                      context, 
+                      RouteWhereYouGo(), 
+                      const Color(0xffffa700), 
+                      Icons.all_inclusive, 
+                      'Example example example', 
+                      RouteWhereYouGo(), 
+                    ),
+                    HomePageCardsGroup(
+                      const Color(0xfff37736), 
+                      Icons.analytics_outlined, 
+                      'Example example', 
+                      context, 
+                      RouteWhereYouGo(), 
+                      const Color(0xffFF6D6D), 
+                      Icons.all_inclusive, 
+                      'Example example example', 
+                      RouteWhereYouGo(), 
+                    ),
+                    HomePageCardsGroup(
+                      const Color(0xfff37736), 
+                      Icons.analytics_outlined, 
+                      'Example example', 
+                      context, 
+                      RouteWhereYouGo(), 
+                      const Color(0xffFF6D6D), 
+                      Icons.all_inclusive, 
+                      'Example example example', 
+                      RouteWhereYouGo(), 
+                    ),
+                    HomePageCardsGroup(
+                      const Color(0xfff37736), 
+                      Icons.analytics_outlined, 
+                      'Example example', 
+                      context, 
+                      RouteWhereYouGo(), 
+                      const Color(0xffFF6D6D), 
+                      Icons.all_inclusive, 
+                      'Example example example', 
+                      RouteWhereYouGo(), 
+                    ),
+
+                    )
+              ],
+            )
+          ],
+        ));
   }
 }
