05dacc778266a22bbfa99a71943bc8a40f0b660a
Tosin8 <tosinezekiel8@gmail.com>
2022-06-28 17:34:11 +0100

    including the singletickerprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(const MaterialApp(home: HomePage()));
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late Animation<double> _animation2;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
